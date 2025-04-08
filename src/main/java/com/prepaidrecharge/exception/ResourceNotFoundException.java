3b3e52f6f4942a058c159b9e2f15c6045ea6e18b
Madhav Priyan S <130158450+Madhavpriyan@users.noreply.github.com>
2025-04-08 15:25:50 +0530

    Delete src directory

--- src/main/java/com/prepaidrecharge/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.prepaidrecharge.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/prepaidrecharge/repository/SupportQueryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.prepaidrecharge.repository;
-
-import com.prepaidrecharge.model.SupportQuery;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface SupportQueryRepository extends JpaRepository<SupportQuery, Integer> {
-    List<SupportQuery> findAllByOrderByQueryDateDesc();
-}
